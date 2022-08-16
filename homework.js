f59adb99b761f2d5e8b6e1c1e50540d0491cd642
Janae Henry <janaedhenry@gmail.com>
2022-08-16 01:42:53 -0400

    Person class created

--- homework.js
+++ homework.js
@@ -2,9 +2,9 @@
 
 class Hamster {
     constructor(owner, nombre, price){
-        this.owner = "",
-        this.nombre = "", 
-        this.price = 15,
+        this.owner = "";
+        this.nombre = ""; 
+        this.price = 15;
     }
 
     wheelRun() {
@@ -15,5 +15,54 @@ class Hamster {
     }
     getPrice() {
         return this.price;
+    }
+}
+
+class Person {
+    constructor(name, age, height, weight, mood, hamsters, bankAccount) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.mood = mood;
+        this.hamsters =[];
+        this.bankAccount = 0;
+
+        const p = new Person("Timmy");
+        p.age = 5;
+    }
+
+   
+
+    getName() {
+        return this.name;
+    }
+    getAge() {
+        return this.age;
+    }
+    getWeight() {
+        return this.weight;
+    }
+    greet() {
+        console.log(this.name)
+    }
+    eat() {
+        this.weight++;
+        this.mood++;
+    }
+    exercise() {
+        this.weight--;
+    }
+    ageUp() {
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankAccount += 10;
+    }
+    buyHamster(Hamster) {
+        this.hamsters.push(Hamster);
+        this.mood += 10;
+        this.bankAccount - getPrice();
+
     }
 }
